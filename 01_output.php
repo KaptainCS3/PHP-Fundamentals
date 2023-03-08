f6a07c7a4d6eef9e7c4ce75483348ed0099ddc5d
KaptainCS3 <mbiapplegryn@gmail.com>
2023-03-08 20:35:55 +0100

    initial commit

---
+++ 01_output.php
@@ -0,0 +1,15 @@
+<?php
+   //this is a single line comment in PHP
+   
+   /* 
+   This is multi line comment in PHP
+   */
+
+//!    output in php
+    //!can be used to print multiple values     
+    echo 1, ' Leonard Welcome to PHP ', 10.4, ' is your result';
+    //!print out only single values    
+    print 24;
+    //!print_r is method use when working with arrays in PHP
+    print_r([1,2,3,4,5,6,7,8,9,10]);
+?>
