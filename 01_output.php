feee2c51a676530d7877ae086a0d2a867dd2a932
KaptainCS3 <mbiapplegryn@gmail.com>
2023-03-09 21:57:08 +0100

    cover output function

--- 01_output.php
+++ 01_output.php
@@ -8,8 +8,16 @@
 //!    output in php
     //!can be used to print multiple values     
     echo 1, ' Leonard Welcome to PHP ', 10.4, ' is your result';
+    
     //!print out only single values    
     print 24;
+
     //!print_r is method use when working with arrays in PHP
     print_r([1,2,3,4,5,6,7,8,9,10]);
+
+    //!returns the info like type and values
+    var_dump('Leonard', ['today', 3, 5.5]);
+    
+    //!output and returns a parsable string representation of the a variable
+    var_export('today');
 ?>
