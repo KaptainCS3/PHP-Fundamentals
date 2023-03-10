2c6cf9206e607064e5cf76da4cf1616499d4529f
KaptainCS3 <mbiapplegryn@gmail.com>
2023-03-10 20:57:12 +0100

    regular, anonymous and arrow function in php

--- 06_functions.php
+++ 06_functions.php
@@ -5,7 +5,29 @@ function greet(){
  echo "good evening Leo";
 }
 
+//! $num1, $num2 are parameters variable to be used in the function
+function sum ($num1, $num2){
+    return $num1 + $num2;
+}
+//! paramater with default values
+function sum_default ($num1 = 8, $num2 = 5){
+    return $num1 + $num2;
+}
+
 //! exe the function
 greet ();
+// 8 & 12 arguments actual value passed to the function
+echo sum(8, 12);
+//! #2
+//! anonymous or function expression
+$welcome = function(){
+    echo 'Welcome Leo how was work today?';
+};
+$welcome();
 
+//! #3
+//! arrow function
+
+$arrow_function = fn() => 'Arrow function as in javascript';
+echo $arrow_function();
 ?>
