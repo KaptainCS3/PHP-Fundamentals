d554c37b1d872e72931622ee6a9e8fef6566e9f4
KaptainCS3 <mbiapplegryn@gmail.com>
2023-03-17 22:21:33 +0100

    started string method

--- 08_string_function.php
+++ 08_string_function.php
@@ -1,3 +1,17 @@
 <?php
 
+    $my_str = "JW.org official website of Jehovah's Witnesses";
+
+    //! strlen method
+    //Returns string length
+    
+    echo strlen("$my_str \n");
+
+    //! strpos method
+    //Returns first occurrence of a substring, and takes two parameter
+    print_r(strpos($my_str, 'website'));
+
+    //! strrev method
+    //Returns string in reverse
+    print_r(strrev($my_str));
 ?>
