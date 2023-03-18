40759b0d7187707451b7639496f2831e930e586d
KaptainCS3 <mbiapplegryn@gmail.com>
2023-03-18 21:19:44 +0100

    complete string method

--- 08_string_function.php
+++ 08_string_function.php
@@ -11,7 +11,25 @@
     //Returns first occurrence of a substring, and takes two parameter
     print_r(strpos($my_str, 'website'));
 
+    //!strrpos method
+    //Returns last occurrence of a substring
+    print_r(strrpos($my_str, "of"));
     //! strrev method
     //Returns string in reverse
     print_r(strrev($my_str));
+
+    //!uppercase
+    echo strtoupper($my_str);
+    
+    //!lowercase
+    echo strtolower($my_str);
+    
+    //! uppercase first letter in string
+    echo ucwords($my_str);
+
+    //!Replace string
+    echo str_replace('JW.org', 'https://www.jw.org', $my_str);
+
+    //!slice a string
+    echo substr($my_str, 0, 18);
 ?>
