35d7898e9bc84e840bd2aadc8582395a70ff65fe
KaptainCS3 <mbiapplegryn@gmail.com>
2023-03-28 22:45:22 +0100

    import utility classes

--- Feed Back/index.php
+++ Feed Back/index.php
@@ -1,16 +1,13 @@
-<?php
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./dist/output.css">
     <title>Document</title>
 </head>
 <body>
-    
+    <h1 class="text-3xl text-red-600">Welcome Testing PHP & Tailwindcss</h1>
 </body>
 </html>
